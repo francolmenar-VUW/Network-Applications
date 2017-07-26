24d0f26a3a7d012b5e45a73efc00303d93ed779e
francolmenar <correofalso97@hotmail.es>
2017-07-26 18:16:20 +1200

    There are the 25 words but no joder
    It repeats letters

--- encode-skel.c
+++ encode-skel.c
@@ -6,21 +6,15 @@
 /* Program developed for NWEN243, Victoria University of Wellington
    Author: Kris Bubendorfer (c) 2015.
    Uses a modified caeser I & II
-
    Compile:  gcc -o encode encode.c
-
    // Make a file with a plain aplhabet in it:
-
    % cat > test
    ABCDEFGHIJKLMNOPQRSTUVWXYZ
    ^D
-
    // test it - this should be the correct result when encoding.
-
    % cat test | encode "i came, i saw"
    key: HJKLNOPQRTUVICAMESWXYZBDFG - 26
    HJKLNOPQRTUVICAMESWXYZBDFG
-
  */
 
 char upcase(char ch){
@@ -30,51 +24,14 @@ char upcase(char ch){
 }
 
 char* fixkey(char* s){
-  int i, j;
-  char plain[26]; // assume key < length of alphabet, local array on stack, will go away!
-
-  for(i = 0, j = 0; i < strlen(s); i++){
-    if(isalpha(s[i])){
-      plain[j++] = upcase(s[i]);
-    }
-  }
-  plain[j] = '\0'; 
-  return strcpy(s, plain);
+/*char* noRepeat (char* key){
+void printLetters(char* word, char letter){
+printf("The character received is: %c\nThe next character in the alphabet is: %c\n",word[0],word[0+1]+1);
 }
 
-/*
-	It deletes the repeated letters of an input string
-	It copy the correct word into a new string which is returned
-	The input string is not changed
-*/
-char* noRepeat (char* key){
-	int boolean = 0;//It represent if the letter is repeated or not
-	int aux = 0;//It is the counter which will be use to know in which position insert the letters in the new string
-	char* check = (char*)malloc(sizeof(char)*strlen(key));// I allocate the maximum amount of memory that the string can have
- 	for(int i = 0; i < strlen(key); i++){// I go through all the key's letters
- 	boolean = 0;//At the beginning of every iteration, we set to false the boolean in order to search for the letter
-  		for(int j = 0; j < strlen(key); j++){// I check all the already checked letters
-    		if (key[i] == check[j]){//It is repeated
-   			boolean = 1;//boolean true
-    		}
-  		}
-  			if(boolean == 0){//It is not repeated
-  				check[aux]=key[i]; //I assign the new letter to the correct key
-  				aux++;
-  			}
-		}
-	return check;
-}
-
-  /* This function needs to build an array of mappings in the 'encode' array from plaintext characters
-  to encypered characters.  The encode array will be indexed by the plaintext char.  To 
-  make this a useful 0-26 index for the array, 'A' will be stubtracted from it (yes you
-  can do this in C).  You can see this in the main(){} below.  The values in the array 
-  will be the cipher value, in the example at the top A -> H, B -> J, etc.
-
-  You are implementing a Caesar 1 & 2 combo Cypher as given in handout.
-  */ 
+  /* This function needs to build an array of mappings in the 'encode' array from plaintext characters 
 void buildtable (char* key, char* encode){
+	//printLetters("hola",'c');
   int keyLenght = strlen(key);//The length of the key
   int mod = 26; //It is the auxiliar variable in order to perform the modular operations
   int aux;
@@ -82,27 +39,34 @@ void buildtable (char* key, char* encode){
   printf("The key is %s and the length is %i\n", key, keyLenght);
   
   fixkey(key); // fix the key, i.e., uppercase and remove whitespace and punctuation
-  printf("The fixed key is %s\n", key);
+  //printf("The fixed key is %s\n", key);
   
   char* check = (char*)malloc(sizeof(char)*strlen(key));// I allocate the maximum amount of memory that the string can have
   strcpy(check, noRepeat(key));
-  printf("The fixed key without repeated letters is %s\n", check);
+  //printf("The fixed key without repeated letters is %s\n", check);
   
   for (int i = 0; i < 26; i++){//loop to set the plaintext letters to the cipher ones
   		if(i == keyLenght-1 ){// We have to place the key without repeated letters here
   			for (int z = 0; z < strlen(check); z++){ //I iterate all the positions of the new key
   				encode[i+z] = check[z];// I copy the key
   			}
-  			printf("Z loop has finished\n");
+  			//printf("Z loop has finished\n");
   			i += strlen(check) ;//I sum the length of the new key in order to know in which position of the decode string I am
   			boole = 1;//boolean to true
   		}
   		else if(boole == 1) {//The key has been copied
      	//printf("position %i\n", i);
+	     	
+ 		for(int z = 1; z < 27; z++){
+ 			printf("%c ",encode[z]);
+ 		}
+ 		
+     	printf("\n");
      	for(int z = 1; z < 27; z++){
      		if( (strchr(encode,encode[i-1]+z)) == NULL ){//The next letter is not in the encode string already
+     				printf("The actual %i character is:%c\tand the previous one is: %c\n",i,encode[i-2],encode[i-3]);
 					printf("Letter %c is introduced in the encoded table\n", encode[i-1]+1);
-					encode[i] = encode[i-1]+z;
+					encode[i-1] = encode[i-2]+z;
 					z = 27;
      		}
      	}
@@ -114,12 +78,6 @@ void buildtable (char* key, char* encode){
   }
 }
 
-/*
-	It checks 
-*/
-int contains (char* word, char letter){
-
-}
 
 int main(int argc, char **argv){
   // format will be: 'program' key {encode|decode}
@@ -157,4 +115,3 @@ int main(int argc, char **argv){
   }
   return 0;
 }
-  
